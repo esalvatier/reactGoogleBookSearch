f1103ac7079679a9199f09003f7cacecc74b27ac
Erik Salvatier <m.erik.salvatier@gmail.com>
2019-01-05 10:14:42 -0800

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <Router>
+        <div>
+          <Navbar />
+          <Wrapper>
+            <Route exact path="/" component={Saved} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/search" component={Search} />
+          </Wrapper>
+          <Footer />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
