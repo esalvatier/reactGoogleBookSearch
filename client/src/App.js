00a54088eaee496eb8eb6359f2b20e16db8394e9
Erik Salvatier <m.erik.salvatier@gmail.com>
2019-01-05 10:21:54 -0800

    changes app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+import Footer from "./components/Footer";
 import logo from "./logo.svg";
 import "./App.css";
 
